44f6df5676fb3bdcbbebd29f3bdd298de17baf2a
10thcode <itsblackgenius@gmail.com>
2024-06-05 17:48:26 +0000

    Add auth.py
    Define require_path decorator to protect private api endpoint from
    unauthorized access.

---
+++ backend/auth.py
@@ -0,0 +1,19 @@
+"""
+Provides a decorator to protect private api endpoint.
+
+require_auth: a decorator to protect private api endpoints
+that need authentication before access.
+"""
+from os import environ
+from flask import Flask, jsonify
+from authlib.integrations.flask_oauth2 import ResourceProtector
+from validator import Auth0JWTBearerTokenValidator
+
+
+require_auth = ResourceProtector()
+
+domain = environ.get("AUTH0_DOMAIN")
+audience = environ.get("AUTH0_API_IDENTIFIER")
+validator = Auth0JWTBearerTokenValidator(domain, audience)
+
+require_auth.register_token_validator(validator)
