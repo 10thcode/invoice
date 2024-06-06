2dabbef5451f74c50e124979236f05fa38256aa1
10thcode <itsblackgenius@gmail.com>
2024-06-06 01:04:32 +0000

    Modify app.py
    Allow CORS for all origins.

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,11 @@ A flask application
 from flask import Flask, jsonify, url_for
 from api.v1.routes import blueprint
 from db import init_db
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 init_db(app)
 app.register_blueprint(blueprint, url_prefix="/api/v1/invoices")
 
