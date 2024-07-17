739dc4ea1f81b9b99e392b2f8218b06554785dd5
10thcode <itsblackgenius@gmail.com>
2024-07-17 14:01:31 +0000

    Remove endpoint to retrieve auth keys

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,6 @@ from flask import Flask, jsonify, url_for
 from api.v1.routes import blueprint
 from db import init_db
 from flask_cors import CORS
-from os import environ
 
 
 app = Flask(__name__)
@@ -22,31 +21,6 @@ def index():
     return jsonify({
         "invoices_url": url_for("blueprint.invoices"),
     }), 200
-
-
-@app.route("/keys/domain", methods=["GET"])
-def domain():
-    """
-    Get Auth0 tenant domain
-    """
-    return environ.get("AUTH0_DOMAIN"), 200
-
-
-@app.route("/keys/client", methods=["GET"])
-def client():
-    """
-    Get Auth0 client ID
-    """
-    return environ.get("AUTH0_CLIENT_ID"), 200
-
-
-@app.route("/keys/audience", methods=["GET"])
-def audience():
-    """
-    Get Auth0 API audience url
-    """
-    return environ.get("AUTH0_AUDIENCE"), 200
-
 
 if __name__ == "__main__":
     app.run()
